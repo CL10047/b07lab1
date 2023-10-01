c4ce9ca68e67a9d54ba66f5e55984ac119c23bd9
Chris Lee <christopher.lee1004@gmail.com>
2023-10-01 16:33:12 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,43 @@
+import java.io.File;
+
 public class Driver {
 	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
+
+		Polynomial poly1 = new Polynomial(new double[]{6, -2, 5}, new int[]{4, 1, 0});
+		Polynomial poly2 = new Polynomial(new double[]{3, 2, 1}, new int[]{0, 1, 2});
+		
+		//add function test
+		Polynomial sum = poly1.add(poly2);
+
+		for (int i = 0; i < sum.coefficients.length; i++) {
+			System.out.print(sum.coefficients[i]);
+			System.out.print(' ');
+			System.out.println(sum.exponents[i]);
+		}
+		System.out.println();
+
+		//product function test
+		Polynomial product = poly1.multiply(poly2);
+		
+		for (int i = 0; i < product.coefficients.length; i++) {
+			System.out.print(product.coefficients[i]);
+			System.out.print(' ');
+			System.out.println(product.exponents[i]);
+		}
+		System.out.println();
+		
+		//read file test
+		File file = new File("/Users/chrislee/b07lab1/f1.txt");
+
+		Polynomial poly3 = new Polynomial(file);
+
+		 for (int i = 0; i < poly3.coefficients.length; i++) {
+			System.out.print(poly3.coefficients[i]);
+			System.out.print(' ');
+			System.out.println(poly3.exponents[i]);
+		}
+
+		//write file test
+		poly3.saveToFile("/Users/chrislee/b07lab1/f3.txt");
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,46 +1,151 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.io.FileReader;
+
 public class Polynomial {
 	double[] coefficients;
-	
+	int[] exponents;
+
 	public Polynomial() {
 		this.coefficients = new double[0];
+		this.exponents = new int[0];
 	}
 
-	public Polynomial(double[] coefficients) {
+	public Polynomial(double[] coefficients, int[] exponents) {
 		this.coefficients = new double[coefficients.length];
+		this.exponents = new int[exponents.length];
 		for (int i = 0; i < coefficients.length; i++) {
 			this.coefficients[i] = coefficients[i];
+			this.exponents[i] = exponents[i];
 		}
+	}
+
+	public Polynomial(File file) {
+		try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String line = reader.readLine();
+
+			String[] terms = line.split("(?=[-+])");
+			this.coefficients = new double[terms.length];
+	        this.exponents = new int[terms.length];
+	        int indexOfVariable;
+
+	        this.coefficients[0] = Double.parseDouble(terms[0]);
+	        this.exponents[0] = 0;
+
+	        for (int i = 1; i < terms.length; i++) {
+	        	indexOfVariable = terms[i].indexOf("x");
+	        	this.coefficients[i] = Double.parseDouble(terms[i].substring(0, indexOfVariable));
+	        	this.exponents[i] = Integer.parseInt(terms[i].substring(indexOfVariable + 1));
+	        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+	public void saveToFile(String filename) {
+		try(PrintStream writer = new PrintStream(filename)) {
+			for(int i = 0; i < this.exponents.length; i++) {
+				if (i == 0) {
+					writer.print(this.coefficients[i]);
+				} else {
+					if (this.coefficients[i] > 0) {
+						writer.print("+");
+					}
+
+					writer.print(coefficients[i]);
+					writer.print("x");
+					writer.print(exponents[i]);
+				}
+			}
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 
 	public Polynomial add(Polynomial p2) {
-		double[] sumC;
-		if(this.coefficients.length >= p2.coefficients.length) {
-			sumC = new double[this.coefficients.length];
-			for (int i = 0; i < this.coefficients.length; i++) {
-				if (i < p2.coefficients.length)
-					sumC[i] = this.coefficients[i] + p2.coefficients[i];
-				else
-					sumC[i] = this.coefficients[i];
-			}
-		} else {
-			sumC = new double[p2.coefficients.length];
-			for (int i = 0; i < p2.coefficients.length; i++) {
-				if (i < this.coefficients.length)
-					sumC[i] = this.coefficients[i] + p2.coefficients[i];
-				else
-					sumC[i] = p2.coefficients[i];
+	    int maxLength = this.coefficients.length + p2.coefficients.length;
+	    int count = 0;
+	    double[] sumC = new double[maxLength];
+	    int[] sumE = new int[maxLength];
+
+	    for (int i = 0; i < this.coefficients.length; i++) {
+	        sumC[this.exponents[i]] = this.coefficients[i];
+	        sumE[this.exponents[i]] = this.exponents[i];
+	    }
+
+	    for (int i = 0; i < p2.coefficients.length; i++) {
+	        if (sumC[p2.exponents[i]] == 0) {
+	        	sumC[p2.exponents[i]] = p2.coefficients[i];
+	        	sumE[p2.exponents[i]] = p2.exponents[i];
+	        } else {
+	        	sumC[p2.exponents[i]] += p2.coefficients[i];
+	        }
+	    }
+
+	    for (int i = 0; i < maxLength; i++) {
+    		if (sumC[i] != 0) {
+    			count++;
+    		}
+    	}
+
+		Polynomial returnP = depopulate(new Polynomial(sumC, sumE), count);
+    	return returnP;
+	}
+
+	public Polynomial multiply(Polynomial p2) {
+		int maxLength = this.exponents.length * p2.exponents.length;
+		int count = 0;
+
+		double[] productC = new double[maxLength];
+    	int[] productE = new int[maxLength];
+
+    	for (int i = 0; i < this.exponents.length; i++) {
+    		for (int j = 0; j < p2.exponents.length; j++) {
+    			double newCoeff = this.coefficients[i] * p2.coefficients[j];
+    			int newExp = this.exponents[i] + p2.exponents[j];
+
+    			if (productE[newExp] == 0) {
+    				productC[newExp] = newCoeff;
+    				productE[newExp] = newExp;
+    			} else {
+    				productC[newExp] += newCoeff;
+    			}
+    		}
+    	}
+
+    	for (int i = 0; i < maxLength; i++) {
+    		if (productC[i] != 0) {
+    			count++;
+    		}
+    	}
+
+	    Polynomial returnP = depopulate(new Polynomial(productC, productE), count);
+    	return returnP;
+	}
+
+	public Polynomial depopulate(Polynomial p, int usedCells) {
+		double[] depC = new double[usedCells];
+    	int[] depE = new int[usedCells];
+    	int count = 0;
+
+		for (int i = 0; i < p.coefficients.length; i++) {
+			if (p.coefficients[i] != 0) {
+				depC[count] = p.coefficients[i];
+				depE[count] = p.exponents[i];
+				count++;
 			}
 		}
 
-		Polynomial returnP = new Polynomial(sumC);
-		return returnP;
+		return new Polynomial(depC, depE);
 	}
 
 	public double evaluate(double x) {
 		double result = 0.0;
 
 		for (int i = 0; i < this.coefficients.length; i++) {
-			result += coefficients[i] * Math.pow(x, i);
+			result += coefficients[i] * Math.pow(x, exponents[i]);
 		}
 		return result;
 	}
